26ab18c134b8935974bb643569f5ae816f1e5592
valerieponce <vponcelu@calpoly.edu>
2025-10-07 22:30:19 -0700

    up to POST

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -52,6 +52,29 @@ app.get("/users", (req, res) => {
     res.send(users);
   }
 });
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
 
 app.listen(port, () => {
   console.log(
