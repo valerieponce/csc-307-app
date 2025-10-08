66bfe0335c2348355e4c43c2961097b71c4c5dc6
valerieponce <vponcelu@calpoly.edu>
2025-10-07 17:20:54 -0700

    add find by name

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -36,8 +36,21 @@ const users = {
   ]
 };
 
-app.get("/users", (req,res)=>{
+const findUserByName = (name) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name
+  );
+};
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
     res.send(users);
+  }
 });
 
 app.listen(port, () => {
